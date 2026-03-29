c8d49bf6386dc5249d2765fc0be9eb9176c88780
cepages <cpages.tr@gmail.com>
2026-03-29 16:08:00 +0200

    Initial Commit

---
+++ PruebaTecnicaAlten/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaTecnicaAlten
+//
+//  Created by Carlos Pagés on 29/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
